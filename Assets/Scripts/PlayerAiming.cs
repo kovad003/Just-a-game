89e349aa1bbe7f2207824aa6da4eabd5fd53e278
kovad003 <kovacsdansan@gmail.com>
2022-11-14 00:47:51 +0200

    Player can aim with RMB

--- Assets/Scripts/PlayerAiming.cs
+++ Assets/Scripts/PlayerAiming.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerAiming : MonoBehaviour
 {
-    public float turnSpeed = 15;
+    public float turnSpeed = 15.0f;
+    public float aimDuration = 0.3f;
     private Camera _mainCamera;
+    [SerializeField] private Rig aimLayer;
     
     // Start is called before the first frame update
     void Start()
@@ -21,5 +25,17 @@ public class PlayerAiming : MonoBehaviour
         float playerCamera = _mainCamera.transform.rotation.eulerAngles.y;
         // Camera will blend in (on the y-axis) from current rotation towards the camera's rotation.
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, playerCamera, 0), turnSpeed * Time.fixedDeltaTime );
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            aimLayer.weight += Time.deltaTime / aimDuration;
+        }
+        else
+        {
+            aimLayer.weight -= Time.deltaTime / aimDuration;
+        }
     }
 }
