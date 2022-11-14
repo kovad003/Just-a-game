8d070c41f1cb027630aaef4890b0bfab9b5b5632
kovad003 <kovacsdansan@gmail.com>
2022-11-14 04:12:02 +0200

    AIming was improved
     - Weapon can be holsetered/unholstered
     - Animation was addded to holster a gun

---
+++ Assets/Scripts/HolsterWeapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolsterWeapon : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/PlayerAiming.cs
+++ Assets/Scripts/PlayerAiming.cs
@@ -1,22 +1,30 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
+/// <summary>
+/// AUTHOR: @Daniel K.
+/// </summary>
 public class PlayerAiming : MonoBehaviour
 {
-    public float turnSpeed = 15.0f;
-    public float aimDuration = 0.3f;
-    private Camera _mainCamera;
+    [SerializeField] private float turnSpeed = 15.0f;
+    [SerializeField] private float aimDuration = 0.3f;
     [SerializeField] private Rig aimLayer;
-    
+    [SerializeField] private GameObject rigLayers;
+    [SerializeField] private GameObject holsteredPistol;
+    private Camera _mainCamera;
+    private RigBuilder _rigBuilder;
+    private Animator _animator;
+    /**************************************************************************************************************/
     // Start is called before the first frame update
     void Start()
     {
         _mainCamera = Camera.main;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        _rigBuilder = GetComponent<RigBuilder>();
+        _animator = GetComponent<Animator>();
     }
     
     // FixedUpdate() must be used bc player has physics and rigidbody.
@@ -24,12 +32,20 @@ public class PlayerAiming : MonoBehaviour
     {
         float playerCamera = _mainCamera.transform.rotation.eulerAngles.y;
         // Camera will blend in (on the y-axis) from current rotation towards the camera's rotation.
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, playerCamera, 0), turnSpeed * Time.fixedDeltaTime );
+        transform.rotation = Quaternion.Slerp(transform.rotation, 
+            Quaternion.Euler(0, playerCamera, 0), turnSpeed * Time.fixedDeltaTime );
     }
 
+    // Update is enough for scanning user input.
     private void Update()
     {
-        if (Input.GetMouseButton(1))
+        HolsterGun(KeyCode.H);
+        MouseAim(Input.GetMouseButton(1));
+    }
+    /**************************************************************************************************************/
+    private void MouseAim(bool mouseInput)
+    {
+        if (mouseInput)
         {
             aimLayer.weight += Time.deltaTime / aimDuration;
         }
@@ -37,5 +53,41 @@ public class PlayerAiming : MonoBehaviour
         {
             aimLayer.weight -= Time.deltaTime / aimDuration;
         }
+    }
+    
+    private void HolsterGun(KeyCode key)
+    {
+        if (Input.GetKeyUp(key))
+        {
+            if (_rigBuilder.enabled)
+            {
+                // _rigBuilder.enabled = false;
+                StartCoroutine(DisableRigBuilder());
+                rigLayers.SetActive(false);
+                _animator.SetTrigger("HolsterPistol");
+                holsteredPistol.SetActive(true);
+            }
+            else
+            {
+                _animator.SetTrigger("UnholsterPistol");
+                holsteredPistol.SetActive(false);
+                rigLayers.SetActive(true);
+                // _rigBuilder.enabled = true;
+                StartCoroutine(EnableRigBuilder());
+            }
+        }
+    }
+
+    // The delay makes the transition better between animation states!
+    // Events weren't working really well bc of the rig layers!
+    IEnumerator EnableRigBuilder()
+    {
+        yield return new WaitForSeconds(0.6f);
+        _rigBuilder.enabled = true;
+    }
+    IEnumerator DisableRigBuilder()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _rigBuilder.enabled = false;
     }
 }
