bdd7ac6e756a1500d8de61018d649ab36eede656
kovad003 <kovacsdansan@gmail.com>
2022-11-15 01:07:23 +0200

    Chase Radius added to the enemy

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,24 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.ProBuilder;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float chaseRadius = 10;
     private NavMeshAgent _navMeshAgent;
+    private float _distanceToTarget = Mathf.Infinity;
     
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // Binding Components:
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        _navMeshAgent.SetDestination(target.position);
+        _distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (chaseRadius <= _distanceToTarget)
+            _navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
